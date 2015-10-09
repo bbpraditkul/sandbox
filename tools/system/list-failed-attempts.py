bf7190a966bcef4d2b6245f5cbcaf564196f1428
Bryan Praditkul <bbpraditkul@gmail.com>
2015-10-08 22:45:32 -0700

    quick ugly script since I'm useless at sed/awk

---
+++ tools/system/list-failed-attempts.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import re
+import os
+from collections import defaultdict
+
+"""
+The following script is a quick hack to review the secure log and roughly summarize:
+    1) failed logins
+    2) origins
+    3) date/time
+"""
+file_name = '/var/log/secure'
+
+os.system('uname -a')
+
+all_failed_users = defaultdict(list)
+
+for log_line in open(file_name):
+    s = [('jordan', 1), ('sissel', 2), ('jordan', 3)]
+
+    m = re.search('^(\S+\s\d{2}\s\d{2}:\d{2}:\d{2}).*Failed password.*user\s+(\S+)\sfrom\s(\d+\.\d+\.\d+\.\d+).*', log_line)
+    if m:
+        all_failed_users[m.group(2) + ' ' + m.group(3)].append(m.group(1))
+
+print "%30s %6s %s" % ("USER", "FAILS", "DATE")
+for key, value in all_failed_users.items():
+    print "%30s %6d %s" % (key, len(value), value)
