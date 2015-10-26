6ff3b22625a6b584014ebd3849db5f529f1b2645
Bryan Praditkul <bbpraditkul@gmail.com>
2015-10-26 10:02:25 -0700

    simple hack to create dynamic blocks for a custom config file

---
+++ code-practice/python/repeat-blocks.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""
+The following hack repeats a preformatted block, with a why dynamic values...simple
+"""
+
+filename = "block-output.txt"
+val1_step_interval = 3
+val2_step_interval = 7
+
+
+def constructBlock(multiplier):
+    out = []
+    out.append("{")
+    out.append("   custom vhost definition id:" + str(multiplier))
+    out.append("   timeout: " + str(val1_step_interval) )
+    out.append("   foreign_id:" + str(val2_step_interval*multiplier))
+    out.append("}")
+    out.append("")
+    return '\n'.join(out)
+
+f = open(filename, 'wb')
+for i in range(1,10):
+    #print (i, ": ", constructBlock(), file=filename)  #python 3.x
+    print >> f, constructBlock(i)
+
+
