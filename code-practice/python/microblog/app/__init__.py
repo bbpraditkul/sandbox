5ed612e091d6d3734eef6d90506c3866c6d783ab
Bryan Praditkul <bbpraditkul@gmail.com>
2016-08-02 22:33:58 -0700

    webforms to flask

--- code-practice/python/microblog/app/__init__.py
+++ code-practice/python/microblog/app/__init__.py
@@ -4,7 +4,9 @@ from flask import Flask
 
 #app_var used here is the name of the variable that's being assigned of the Flask class
 app_var = Flask(__name__)
+app_var.config.from_object('config') #start using the config
 
 #app used here is the package where we're importing the "views" module
+
 from app import views
 
--- code-practice/python/microblog/app/views.py
+++ code-practice/python/microblog/app/views.py
@@ -1,5 +1,6 @@
 from app import app_var
-from flask import render_template #templates
+from flask import render_template, flash, redirect #templates: render_template, forms: flash,redirect
+from .forms import LoginForm
 
 #simple view function
 
@@ -7,11 +8,22 @@ from flask import render_template #templates
 
 @app_var.route('/')
 @app_var.route('/index')
-
+@app_var.route('/login', methods=['GET', 'POST'])
 #basic "hello world" example
 #def index():
 #    return "This is a test page..."
 #
+
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form,
+                           providers=app_var.config['OPENID_PROVIDERS'])
 
 #working with templates
 def index():
