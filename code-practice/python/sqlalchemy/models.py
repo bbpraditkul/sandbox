2f07b36064849776b9cf30e7c4e075001c220666
Bryan Praditkul <bbpraditkul@gmail.com>
2016-08-15 10:30:16 -0700

    still trying to figure out the db table creation

--- code-practice/python/sqlalchemy/models.py
+++ code-practice/python/sqlalchemy/models.py
@@ -1,10 +1,19 @@
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+from app import app
 
-app = Flask(__name__)
+"""
+run in ipython:
+>>> from app import app
+>>> from models import db
+>>> db.create_all()
+
+"""
+
+
 db = SQLAlchemy(app)
 
 class TestTable (db.Model):
     __tablename__ = "testtable"
-     id = db.Column('id', db.Integer, primary_key=True)
-     name = db.Column('name', db.Unicode)
-     value = db.Column('value', db.Integer)
+    id = db.Column('id', db.Integer, primary_key=True)
+    name = db.Column('name', db.Unicode)
+    value = db.Column('value', db.Integer)
