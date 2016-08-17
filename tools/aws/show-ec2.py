fcb4a751e123efc6816b990ffa96355b64a58488
Bryan Praditkul <bbpraditkul@gmail.com>
2016-08-17 00:35:32 -0700

    updated for python3

--- tools/aws/show-ec2.py
+++ tools/aws/show-ec2.py
@@ -7,13 +7,13 @@ regions = ['us-west-1', 'us-west-2', 'us-east-1']
 client = boto3.client('ec2')
 
 for region in regions:
-    print region
+    print (region)
     ec2 = boto3.resource('ec2', region)
     tag = ec2.Tag('resource_id','key','value')
 
     instances = ec2.instances.filter(
         Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
-    print "ID, PRV_DNS, PRV_IP, PUB_DNS, PUB_IP, TAGS"
+    print ("ID, PRV_DNS, PRV_IP, PUB_DNS, PUB_IP, TAGS")
     for instance in instances:
         print(instance.id, instance.private_dns_name, instance.private_ip_address, instance.public_dns_name, instance.public_ip_address, instance.tags)
 
